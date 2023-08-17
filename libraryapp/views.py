5c05e6a4d1e9822dc3b0e8733761fd333e49c337
unknown <rnishant2796@gmail.com>
2023-08-17 23:33:29 +0530

    index page

--- libraryapp/views.py
+++ libraryapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def books(request):
+    print(request.path)
     return render(request, "index.html")
 
 def issued_books(request):
