46ca0e3a929a848f0a44de4e60b891da796e4778
Nishant <rnishant2796@gmail.com>
2023-08-21 18:50:07 +0530

    issue books

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('<str:filterBy>/', books, name='home'),
     path('', books, name='home'),
-    path('books', issued_books, name='books')
+    path('books', issued_books, name='books'),
+    path('users', search_user , name='search_user'),
+    path('assign_book', assign_book , name='assign_book')
 ]
--- libraryapp/views.py
+++ libraryapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Book
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+
+
+@login_required(login_url="login")
 def books(request, filterBy=None):
     books = Book.objects.all()
     if filterBy:
@@ -10,3 +15,14 @@ def books(request, filterBy=None):
 
 def issued_books(request):
     return render(request, "issuedbooks.html")
+
+
+def search_user(request):
+    user = request.GET.get("user_name", None)
+    if user:
+        user_obj = list(User.objects.filter(first_name__icontains=user).values("first_name"))
+        return JsonResponse({"users":user_obj, "status":True})
+    return JsonResponse({"users":user_obj, "status":False})
+
+def assign_book(request):
+    return render(request, "bookassign.html", {"books":books})
